11fd9eb0fae4315ed9f7ab95b7a099684421d264
sonnv-1936 <nguyen.van.son@sun-asterisk.com>
2019-05-28 11:11:44 +0700

    add log in log out function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if user.save
+      log_in user
       flash[:success] = "You have signed up successfully!"
-      redirect_to user
+      redirect_to root_path
     else
       render :new
     end
